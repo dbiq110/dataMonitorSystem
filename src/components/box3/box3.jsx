2c68d12eb28d43433884a7c0f8225897712bf5c8
locky <735614908@qq.com>
2019-03-13 08:12:04 +0800

    little style to change

--- src/components/box3/box3.jsx
+++ src/components/box3/box3.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import './box3.less'
  const Box3=()=>{
   return <ul className="box3">
       <li>
-        <p className="title"></p>
+        <img className="icon-up" src={require('../../assets/image/icon/icon-up.png')} alt=""/>
+        <p className="title">不知道啥50%</p>
         <p className="content-box">
           <i className="background"><i className="deep"></i></i>
           <span className="decs">1200</span>
